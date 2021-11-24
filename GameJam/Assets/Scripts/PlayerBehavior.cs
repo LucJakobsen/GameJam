4a85bc2ef08a0c5895e10f97e98fbc27229e4675
LucJakobsen <lucjakobsen@gmail.com>
2021-11-24 09:21:57 +0100

    did some cool stuff

--- GameJam/Assets/Scripts/PlayerBehavior.cs
+++ GameJam/Assets/Scripts/PlayerBehavior.cs
@@ -7,11 +7,18 @@ public class PlayerBehavior : MonoBehaviour
     Rigidbody rb;
 
     bool isGrounded = false;
+    bool isJumping;
+    float groundDistance = 0.7f;
 
     float moveSpeed = 5.0f;
 
     float jumpHeight = 0.0f;
-    float dashForce;
+    int counter = 0;
+
+    [SerializeField]private float dashForce = 50f;
+    private CapsuleCollider col;
+
+    public LayerMask groundLayer;
 
     RaycastHit Hit;
     int soulsCollected;
@@ -19,61 +26,83 @@ public class PlayerBehavior : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
     }
 
     private void Update()
     {
-        CheckGrounded();
-        Debug.Log(isGrounded);
+        isOnGround();
         Move();
         Jump();
         //FaceForward();
         PickUp();
     }
 
-    void CheckGrounded()
+   // void CheckGrounded()
+   // {
+    //    Ray ray = new Ray(transform.position, Vector2.down);
+    //    if (Physics.Raycast(ray, 0.9f))
+     //   {
+     //       isGrounded = true;
+     //   }
+     //   else isGrounded = false;
+   // }
+
+    void isOnGround()
     {
-        Ray ray = new Ray(transform.position, Vector2.down);
-        if (Physics.Raycast(ray, 0.7f))
+        Vector3 capsuleBottom = new Vector3(col.bounds.center.x,
+                  col.bounds.min.y, col.bounds.center.z);
+
+        isGrounded = Physics.CheckCapsule(col.bounds.center,
+           capsuleBottom, groundDistance, groundLayer,
+              QueryTriggerInteraction.Ignore);
+    }
+
+    void FaceForward()
+    {
+        Vector3 vel = rb.velocity;
+        if (vel.x != 0 )
         {
-            isGrounded = true;
+            transform.forward = vel;
         }
-        else isGrounded = false;
     }
 
     void Move()
     {
-        if(isGrounded && !Input.GetKey(KeyCode.Space))
+        if(!Input.GetKey(KeyCode.Space) && isGrounded)
         {
             float hInput = Input.GetAxis("Horizontal") * moveSpeed;
             rb.velocity = new Vector2(hInput, rb.velocity.y);
-
-           // if (rb.velocity.x != 0)
-            //{
-             //   transform.forward = rb.velocity;
-           // }
         }
-    }
+    }   
 
     void Jump()
     {
-
         if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            if (jumpHeight <= 7) {
-                jumpHeight+= 0.05f;
+            if (jumpHeight <= 15) {
+                jumpHeight+= 0.1f;
+                counter = 0;
             }
         }
 
         if (isGrounded && Input.GetKeyUp(KeyCode.Space))
         {
-
+            
             rb.AddForce(Vector2.up * jumpHeight, ForceMode.Impulse);
-            Invoke("ResetJump", 0.2f);
+            Invoke("ResetJump", 0.4f);
+
         }
-    
-        
-    }
+
+       if (Input.GetKey(KeyCode.A) && !isGrounded && counter == 0 )
+            {
+            rb.AddForce(transform.right * -dashForce);
+            counter++;
+            } else if (Input.GetKey(KeyCode.D) && counter == 0) {
+            rb.AddForce(transform.right * dashForce);
+            counter++;
+            }
+        }
 
     private void ResetJump()
     {
